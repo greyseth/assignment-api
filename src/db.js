ee351d6c441ca63d2251ee6ad08f1bd61db6f448
greyseth <anargya2gilland@gmail.com>
2023-11-05 22:35:12 +0700

    added separate keys file

--- src/db.js
+++ src/db.js
@@ -1,8 +1,6 @@
 const db = require("@supabase/supabase-js");
+const { url, key } = require("./keys");
 
-const connection = db.createClient(
-  "https://vqjkwmcimjeuokiukobz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxamt3bWNpbWpldW9raXVrb2J6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzOTE0MTQsImV4cCI6MjAxMzk2NzQxNH0.38LZjZiPemSTDW7cP0UC8heKe7h1JG8WXD03p6yasjU"
-);
+const connection = db.createClient(url, key);
 
 module.exports = connection;
